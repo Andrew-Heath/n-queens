15d2251ea38801c10c62b16d5995bdcda0c2eaae
Andrew-Heath <aheath42@gmail.com>
2016-06-15 15:53:46 -0700

    Handle diagonal conflicts

--- src/Board.js
+++ src/Board.js
@@ -132,12 +132,28 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var found = false;
+      for (var row = 0, col = majorDiagonalColumnIndexAtFirstRow; this._isInBounds(row, Math.abs(col)); row++, col++) {
+        if (col >= 0) {
+          if (this.rows()[row][col]) {
+            if (found) {
+              return true;
+            }
+            found = true;
+          }
+        }
+      }
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var i = -(this.get('n') - 1); Math.abs(i) < this.get('n'); i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        } 
+      }
+      return false;
     },
 
 
@@ -147,12 +163,29 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var found = false;
+      for (var row = 0, col = minorDiagonalColumnIndexAtFirstRow; this._isInBounds(row, Math.abs(col - 3)); row++, col--) {
+        if (col >= 0) {
+          if (this.rows()[row][col]) {
+            if (found) {
+              return true;
+            }
+            found = true;
+          }
+        }
+      }
+      return false;
+
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      for (var i = 2 * (this.get('n') - 1); Math.abs(i - 3) < this.get('n'); i--) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        } 
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
